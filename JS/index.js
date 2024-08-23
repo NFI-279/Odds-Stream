e1733ca7861b1620e82eff9e4263dd1b295cfea7
NFI-279 <78752610+NFI-279@users.noreply.github.com>
2024-08-23 20:17:17 +0300

    first commit

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+function displayLogin(){
+    // Make sure registration form is closed
+    // Create Log in Form
+    // Display Log in Form
+}
+function displayRegister(){
+    // Make sure log in form is closed
+    // Create Registration Form
+    // Display Registration Form
+}
+
+const loginButton = document.getElementById("loginButton");
+const registerButton = document.getElementById("registerButton");
+
+loginButton.addEventListener('click', function(){
+    console.log("Login Listener");
+    
+});
