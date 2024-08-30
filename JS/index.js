c4e7baa23d5bdc9d3cb7c0214b94618a38352917
NFI-279 <78752610+NFI-279@users.noreply.github.com>
2024-08-30 14:20:25 +0300

    Added Login & Register Modals

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+const loginContainer = document.getElementById("formContainer");
+const registerContainer = document.getElementById("registerContainer");
+const subtitle = document.getElementById("secondSubtitle");
+const container = document.getElementById("mainContainer");
 function displayLogin(){
-    // Make sure registration form is closed
-    // Create Log in Form
-    // Display Log in Form
+    formContainer.style.display = "block";
+    registerContainer.style.display = "none";
+    mainContainer.classList.add("modal-blur");
+    secondSubtitle.classList.add("modal-blur");
 }
 function displayRegister(){
-    // Make sure log in form is closed
-    // Create Registration Form
-    // Display Registration Form
+    registerContainer.style.display = "block";
+    formContainer.style.display = "none";
+    mainContainer.classList.add("modal-blur");
+    secondSubtitle.classList.add("modal-blur");
 }
 
-const loginButton = document.getElementById("loginButton");
-const registerButton = document.getElementById("registerButton");
+function closeModal() {
+    formContainer.style.display = "none";
+    registerContainer.style.display = "none";
+    container.classList.remove("modal-blur");
+    subtitle.classList.remove("modal-blur"); 
+    console.log("Test"); 
+}
 
-loginButton.addEventListener('click', function(){
-    console.log("Login Listener");
-    
+    // You can attach the event listeners to buttons here if needed, like this:
+    document.getElementById("loginButton").onclick = displayLogin;
+    document.getElementById("registerButton").onclick = displayRegister;
+    document.getElementById("closeLogin").onclick = closeModal;
+    document.getElementById("closeRegister").onclick = closeModal;
 });
